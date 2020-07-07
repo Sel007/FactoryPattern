6e8325d4d2c1042a88a470059f360ceaed62b91f
Sel007 <selina.08225.1@gmail.com>
2020-07-07 18:34:41 -0400

    Add project files.

---
+++ FactoryPatternConsoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FactoryPatternConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
