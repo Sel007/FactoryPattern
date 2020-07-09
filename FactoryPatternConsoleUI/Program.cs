43924c5cd4c74000d6c4461416d94286003f0f2f
Sel007 <selina.08225.1@gmail.com>
2020-07-08 23:29:36 -0400

    Completed

--- FactoryPatternConsoleUI/Program.cs
+++ FactoryPatternConsoleUI/Program.cs
@@ -6,7 +6,19 @@ namespace FactoryPatternConsoleUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int wheelCount;
+            bool input = false;
+
+            do
+            {
+                Console.WriteLine("Enter the amount of tires for the vehicle you want to create: ");
+
+                input = int.TryParse(Console.ReadLine(), out wheelCount);
+            } while (!input);
+
+            var vehicle = VehicleFactory.GetVehicle(wheelCount);
+
+            vehicle.Drive();
         }
     }
 }
